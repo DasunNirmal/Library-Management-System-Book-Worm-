4339d734f38ea81ce9110b57fb1bf967c75d8f5b
DasunNirmal <dasun200301@gmail.com>
2024-03-08 17:57:59 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/controller/LoginFxmlController.java
@@ -0,0 +1,39 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class LoginFxmlController {
+
+    @FXML
+    private PasswordField txtPasswordField;
+
+    @FXML
+    private TextField txtPasswordTextField;
+
+    @FXML
+    private CheckBox checkBox;
+
+    @FXML
+    private ImageView imgLock;
+
+
+    @FXML
+    void changeVisibilityOnAction(ActionEvent event) {
+        if (checkBox.isSelected()) {
+            txtPasswordTextField.setText(txtPasswordField.getText());
+            txtPasswordTextField.setVisible(true);
+            txtPasswordField.setVisible(false);
+            imgLock.setVisible(false);
+            return;
+        }
+        txtPasswordField.setText(txtPasswordTextField.getText());
+        txtPasswordField.setVisible(true);
+        txtPasswordTextField.setVisible(false);
+        imgLock.setVisible(true);
+    }
+}
