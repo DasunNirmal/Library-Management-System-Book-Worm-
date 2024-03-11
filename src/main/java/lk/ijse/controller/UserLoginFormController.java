2f2d275308b037f9dcca30bc1b389142f278a5f5
DasunNirmal <dasun200301@gmail.com>
2024-03-11 15:43:49 +0530

    Navigate to Register Page

--- src/main/java/lk/ijse/controller/UserLoginFormController.java
+++ src/main/java/lk/ijse/controller/UserLoginFormController.java
@@ -2,10 +2,16 @@ package lk.ijse.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class UserLoginFormController {
 
@@ -21,6 +27,9 @@ public class UserLoginFormController {
     @FXML
     private ImageView imgLock;
 
+    @FXML
+    private AnchorPane rootNode;
+
 
     @FXML
     void changeVisibilityOnAction(ActionEvent event) {
@@ -35,5 +44,16 @@ public class UserLoginFormController {
         txtPasswordField.setVisible(true);
         txtPasswordTextField.setVisible(false);
         imgLock.setVisible(true);
+    }
+
+    @FXML
+    void hypRegistrationOnAction(ActionEvent event) throws IOException {
+        AnchorPane anchorPane = FXMLLoader.load(this.getClass().getResource("/view/registration_form.fxml"));
+        Scene scene = new Scene(anchorPane);
+        Stage stage = (Stage) this.rootNode.getScene().getWindow();
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.setResizable(false);
+        stage.setTitle("Register");
     }
 }
